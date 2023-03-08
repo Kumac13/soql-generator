68abb85a595610e0f84fd4b01eac066c99cf1649
kumac13 <foohilack@gmail.com>
2023-03-09 08:51:02 +0900

    feat: user rustyline

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,58 @@ mod helper;
 mod salesforce;
 
 use helper::DynError;
-use std::io::{self, Write};
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
 use tokio::runtime::Runtime;
 
 fn main() -> Result<(), DynError> {
+    let mut rl = DefaultEditor::new()?;
+    #[cfg(feature = "with-file-history")]
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+
     println!("Welcome to SOQL Generator");
     println!("Type 'exit' to quit");
     loop {
-        print!("SOQLGenerator >>> ");
-        io::stdout().flush().unwrap();
+        let readline = rl.readline("SOQLGenerator >>> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
 
-        let mut expr = String::new();
-        io::stdin().read_line(&mut expr).unwrap();
+                if line.trim() == "exit" {
+                    break;
+                }
 
-        if expr.trim() == "exit" {
-            break;
-        }
+                let (query, open_browser) = match engine::build_query(&line) {
+                    Ok(v) => v,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        continue;
+                    }
+                };
 
-        let (query, open_browser) = match engine::build_query(&expr) {
-            Ok(v) => v,
-            Err(e) => {
-                eprintln!("{}", e);
-                continue;
+                let rt = Runtime::new().unwrap();
+                rt.block_on(salesforce::run(&query, open_browser)).unwrap();
             }
-        };
-
-        let rt = Runtime::new().unwrap();
-        rt.block_on(salesforce::run(&query, open_browser)).unwrap();
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
     }
+    #[cfg(feature = "with-file-history")]
+    rl.save_history("history.txt").unwrap_or_else(|e| {
+        eprintln!("Failed to save history: {e}");
+    });
 
     Ok(())
 }
