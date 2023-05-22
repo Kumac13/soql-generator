a6e0f69e8da0d135e6fd3eb04cc57f9242a90e9a
kumac13 <foohilack@gmail.com>
2023-05-22 11:59:27 +0900

    feat: use new parse

--- src/engine.rs
+++ src/engine.rs
@@ -5,6 +5,9 @@ mod parser;
 mod querygen;
 mod token;
 
+use crate::engine::lexer::tokenize;
+use crate::engine::parse::Parser;
+use crate::engine::querygen::Query;
 use crate::helper::DynError;
 
 pub fn print(expr: &str) -> Result<(), DynError> {
@@ -21,8 +24,15 @@ pub fn print(expr: &str) -> Result<(), DynError> {
 }
 
 pub fn build_query(expr: &str) -> Result<(String, bool), DynError> {
-    let query = parser::parse(expr)?;
+    let tokens = tokenize(expr);
+    let mut parser = Parser::new(tokens);
+    let program = parser.parse()?;
+
+    let mut query = Query::default();
+    query.evaluate(program)?;
     let generated_code = query.generate();
+
+    println!("generated query: {}", generated_code);
 
     Ok((generated_code, query.open_browser))
 }
